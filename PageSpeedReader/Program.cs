d676464ecded57acda57331b5c7aa7523674b2de
helpsdev <ared2728@gmail.com>
2022-03-12 18:36:30 -0700

    Missing program and csproj files

---
+++ PageSpeedReader/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Google.Apis.PagespeedInsights.v5;
+using Google.Apis.PagespeedInsights.v5.Data;
+using Google.Apis.Services;
+using MongoDB.Driver;
+using System.Text.Json;
+
+if (args.Length == 0)
+{
+    throw new ArgumentException("a page url must be provided");
+}
+
+string pageUrl = args[0];
+
+if (string.IsNullOrEmpty(pageUrl))
+{
+    throw new ArgumentException(nameof(pageUrl));
+}
+
+Console.WriteLine("Hello, World!");
+
+var pageSpeedService = new PagespeedInsightsService(new BaseClientService.Initializer
+{
+    ApplicationName = "Page Speed Test",
+    ApiKey = "AIzaSyD6evH0lZ7DhD6Ys8c0RGpM4Q6wRTe-xeQ"
+});
+
+var pageSpeedRequest = pageSpeedService.Pagespeedapi.Runpagespeed(pageUrl);
+var pageSpeedResponse = pageSpeedRequest.Execute();
+
+
+var mongoClient = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+var db = mongoClient.GetDatabase("PageSpeedMetrics");
+
+var speedCollection = db.GetCollection<PagespeedApiPagespeedResponseV5>("PageSpeedResults");
+
+speedCollection.InsertOne(pageSpeedResponse);
+
+Console.WriteLine("Completed!");
+Console.ReadKey();
